0c7c5be670eb338f8c42999f792bda809e100fa2
SorawitSakarin <sorawit.saka@gmail.com>
2022-11-16 16:55:15 +0700

    qrcode

--- src/shared/page/AboutUs.js
+++ src/shared/page/AboutUs.js
@@ -4,22 +4,9 @@ import Button from "../components/FormElements/Button";
 import ReactToPrint from "react-to-print";
 import { Policy } from "./Policy";
 import QRCode from "react-qr-code";
-// const AboutUs = () => {
-//   return (
-//     <div>
-//       <ReactToPrint
-//         trigger={() => <button>Print this out!</button>}
-//         content={() => this.componentRef}
-//         documentTitle="Test Printer"
-//         pageStyle="print"
-//       />
 
-//       <Policy ref={el => (this.componentRef = el)} />
-//     </div>
-// //   );
-// // };
 
-// export default AboutUs;
+
 class AboutUs extends React.PureComponent {
   constructor(props) {
     super(props);
